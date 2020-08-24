15d71c23774734039f10e086e9e49396020f386b
diegoaldarondo@g.harvard.edu <diegoaldarondo@g.harvard.edu>
2020-08-24 13:44:26 -0400

    change multi-gpu-data to work with new batch prediction

--- deps/load_multi_gpu_data.m
+++ deps/load_multi_gpu_data.m
@@ -1,17 +1,15 @@
 function data = load_multi_gpu_data(filepath)
 %%
-files = dir(fullfile(filepath,'*MAX*.mat'));
-Num = cellfun(@str2double, regexp({files.name},'\d*','Match'));
-[~, I] = sort(Num);
+files = dir(fullfile(filepath,'*AVG*.mat'));
+num = cellfun(@str2double, regexp({files.name},'\d*','Match'), 'uni', 0);
+files(cellfun(@isempty, num)) = [];
+num = cell2mat(num(~cellfun(@isempty, num)));
+[~, I] = sort(num);
 files = files(I);
 
+clear data
 for nFile = 1:numel(files)
-    disp(nFile)
     data(nFile) = load(fullfile(files(nFile).folder, files(nFile).name));
 end
-out = zeros(size(data(end).pred));
-for nFile = 1:numel(files)
-    out(1+(nFile-1)*1000:nFile*1000,:,:) = data(nFile).pred(1:1000,:,:);
-end
 
-
+data = cat(1, data.pred);
