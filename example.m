468fa32d3cfc0be14e7d8e8d059621faf84cd41a
Diego Aldarondo <diegoaldarondo@gmail.com>
2023-06-12 16:00:37 -0400

    example.m

--- example.m
+++ example.m
@@ -33,13 +33,16 @@ addpath(genpath('skeletons'))
 danncePath = 'Y:/Diego/code/DANNCE';
 
 %% Load in the calibration parameter data
-projectFolder = fullfile(danncePath,'demo/markerless_mouse_1');
+% projectFolder = fullfile(danncePath,'demo/markerless_mouse_1');
+projectFolder = 'Y:/Everyone/dannce_rig/dannce_ephys/art/2020_12_23_1';
+projectFolder = "./social_videos";
+% projectFolder = fullfile(danncePath,'demo/markerless_mouse_1');
 calibPaths = collectCalibrationPaths(projectFolder);
 params = cellfun(@(X) {load(X)}, calibPaths);
 
 %% Load the videos into memory
 vidName = '0.mp4';
-vidPaths = collectVideoPaths(projectFolder,vidName);
+vidPaths = collectVideoPaths('./social_videos',vidName);
 videos = cell(6,1);
 sync = collectSyncPaths(projectFolder, '*.mat');
 sync = cellfun(@(X) {load(X)}, sync);
@@ -47,19 +50,45 @@ sync = cellfun(@(X) {load(X)}, sync);
 % In case the demo folder uses the dannce.mat data format. 
 if isempty(sync)
     dannce_file = dir(fullfile(projectFolder, '*dannce.mat'));
-    dannce = load(fullfile(dannce_file(1).folder, dannce_file(2).name));
+    dannce = load(fullfile(dannce_file(1).folder, dannce_file(1).name));
     sync = dannce.sync;
     params = dannce.params;
 end
 
-framesToLabel = 1:100;
+vidPaths = vidPaths(1:2);
+videos = videos(1:2);
+sync = sync(1:2);
+params = params(1:2);
+
+framesToLabel = 90000:2:92000;
 for nVid = 1:numel(vidPaths)
+    disp(nVid)
     frameInds = sync{nVid}.data_frame(framesToLabel);
-    videos{nVid} = readFrames(vidPaths{nVid}, frameInds+1);
+%     vidoes{nVid} = readFrames(vidPaths{nVid}, frameInds);
+    vid = VideoReader(vidPaths{nVid});
+    for i = 1:numel(frameInds)
+        vid.CurrentTime = (frameInds(i)+1)/50;
+        frame = readFrame(vid);
+        disp(i)
+        if i == 1
+            videos{nVid} = zeros(size(frame,1), size(frame,2), size(frame,3), numel(frameInds), 'uint8');
+        end
+        videos{nVid}(:,:,:,i) = frame;
+    end
+    delete(vid)
 end
+% parfor nVid = 1%:numel(vidPaths)
+%     frameInds = sync{nVid}.data_frame(framesToLabel);
+%     videos{nVid} = readFrames(vidPaths{nVid}, frameInds+1);
+% end
 
 %% Get the skeleton
-skeleton = load('skeletons/rat16');
+% skeleton = load('skeletons/rat16');
+skeleton = load('skeletons/rat23');
+skeleton.joints_idx = cat(1, skeleton.joints_idx, skeleton.joints_idx + 23 );
+skeleton.color = cat(1, skeleton.color, skeleton.color);
+skeleton.joint_names = cat(2, skeleton.joint_names, skeleton.joint_names);
+
 % skeleton = load('com');
 
 %% Start Label3D
@@ -67,12 +96,23 @@ close all
 labelGui = Label3D(params, videos, skeleton);
 % labelGui = Label3D(params, videos, skeleton, 'sync', sync, 'framesToLabel', framesToLabel);
 
+pts3d = load('save_data_AVG.mat');
+pts3d = pts3d.pred(framesToLabel,:,:);
+labelGui.loadFrom3D(pts3d)
+
 %% Check the camera positions
 labelGui.plotCameras       
 
 %% If you just wish to view labels, use View 3D
 close all
 viewGui = View3D(params, videos, skeleton);
+pts3d = load('save_data_AVG.mat');
+% pts3d
+pts3d = permute(pts3d.pred, [1, 3, 4, 2]);
+disp(size(pts3d))
+pts3d = reshape(pts3d, size(pts3d, 1), 3, []);
+pts3d = pts3d(framesToLabel,:,:);
+viewGui.loadFrom3D(pts3d)
 
 %% You can load both in different ways
 close all;
